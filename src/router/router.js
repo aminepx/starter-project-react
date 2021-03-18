dbe321d2d5fb17cbf81ee79daae381932410a9a9
aminepx <aminepx@gmail.com>
2021-03-18 14:20:00 +0000

    login done

--- src/router/router.js
+++ src/router/router.js
@@ -6,9 +6,9 @@ import {
 } from "react-router-dom";
 import Dashboard from "../pages/dashboard-page";
 import LmsPages from "../pages/lms-pages";
-import Login from "../pages/login-page";
+import Login from "./../pages/login/login-page";
 import Error from "../pages/error-page";
-import Register from "../pages/register-pages";
+import Register from "./../pages/register/register-pages";
 
 export default function AppRouter() {
   return (
@@ -16,7 +16,7 @@ export default function AppRouter() {
  
         <Switch>
            <Route exact path="/"component={LmsPages}/>
-           <Route path="/regiter"component={Register}/>
+           <Route path="/register"component={Register}/>
            <Route path="/dash"component={Dashboard}/>
            <Route path="/admin"component={Login}/>
            <Route path="*"component={Error}/>
